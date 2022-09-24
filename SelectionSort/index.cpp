0b2c42eac4babf9e835745f9229acd42ba2a5cf9
2raiyyan1khan <inforaiyyan@gmail.com>
2022-09-24 12:11:06 +0530

    implementation

--- SelectionSort/index.cpp
+++ SelectionSort/index.cpp
@@ -3,8 +3,30 @@ using namespace std;
 
 int main()
 {
+    int size = 5;
+    int arr[5] = {34, 12, 54, 27, 8};
 
-    cout << "Hello Selection Sort" << endl;
+    for (int i = 0; i < size - 1; i++)
+    {
+        // assume i is minimum
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        // Swaping value using builin swap function
+        swap(arr[minIndex], arr[i]);
+    }
+    // Print Array
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+        ;
+    }
 
     return 0;
 }
